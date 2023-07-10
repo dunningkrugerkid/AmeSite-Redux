96a9e75156fff084989208fe93d618928df28b1d
Amélie <amelierb@mun.ca>
2023-07-10 10:15:33 -0230

    visual stuff

--- src/components/Space.tsx
+++ src/components/Space.tsx
@@ -1,23 +1,15 @@
-import {useState} from 'react';
+
 import Star from '../components/Star'
 /*
 The container for all the stars. 
 */
 
 export default function Space() {
-    function randIntInterval(min : number, max : number) {
-        return Math.floor(Math.random() * (max-min + 1) + min);
-    }
-
-    
-    
-    // how many stars to populate presently
-    const [randNum, setRandNum] = useState(randIntInterval(3,6))
 
     return ( 
         <div className = "bg-slate-700 bg-opacity-50 bg-rounded-lg border-2 border-slate-500/50">
             <p>
-                {Array(randNum).fill(<Star/>)}
+                {<Star/>}
             </p>
         </div>
     )
--- src/components/Star.tsx
+++ src/components/Star.tsx
@@ -1,42 +1,50 @@
-import {useState, useEffect} from 'react';
+import {
+    QueryClient,
+    QueryClientProvider,
+    useQuery,
+  } from '@tanstack/react-query';
+import starVisual from '../img/star.gif'
+import starError from '../img/star-error.gif'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+  
+import axios from 'axios';
 /*
 Represents one star. Grabs from the backend to supply data.
 */
 
+const queryClient = new QueryClient();
+
 export default function Star() {
-    const [data, setData] = useState(null);
-    const [loading, setLoading] = useState(true);
 
-    useEffect ( () => {
-        fetch('http://amesite.fly.dev')
-        .then(response=> {
-            if (response.ok) {
-                return response.json()
-            }
-            throw response;
-        })
-        .then(data => {
-            setData(data);
-            console.log("logging...")
-            console.log(data);
-        }).finally( () => {
-            setLoading(false);
-            });
-    });
-
-    if(loading) return (
-        <div>
-        Loading data...
-        </div>
+  return (
+    <QueryClientProvider client={queryClient}>
+      <GetStar />
+      <ReactQueryDevtools initialIsOpen={true} />
+    </QueryClientProvider>
     )
     
-
-    return (
-        <div>
-            <a href="#">loaded<span className="tooltip">test</span></a>
-        </div>
-    )
 }
 
+function GetStar() {
+    const { isLoading, error, data: stars, isFetching } = useQuery({
+        queryKey : ['stars'],
+        queryFn: () => 
+            axios.get('https://amesite.fly.dev')
+            .then((res) => {
+              return res.data;
+            })
+      });
+    
+      if (isLoading) return <img src={starVisual} alt="loading..." />
+    
+      if (error) return <img src={starError} alt="loading..." />
 
-
+      return (
+        <div>
+            <div>
+                {isFetching ? 'Updating...' : ''}
+            </div>
+            {stars}
+        </div>
+      )
+}
